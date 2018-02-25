3a19fd3f55e15cea70fabb0485d93cd6a777fc4f
Dimitar Kumanov <dimitar_kumanov@yahoo.com>
2018-02-25 03:29:11 +0200

    Add one silver sponsor and Facebook metadata and Pixel

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -14,6 +14,7 @@ import droxicLogo from '../assets/partners/droxic.png';
 import scalefocusLogo from '../assets/partners/scalefocus.png';
 
 // Silver
+import centroidaLogo from '../assets/partners/centroida.png';
 import coresNetworksLogo from '../assets/partners/cores_networks.png';
 import sbtechLogo from '../assets/partners/sbtech.png';
 import strypesLogo from '../assets/partners/strypes.png';
@@ -30,6 +31,7 @@ import peikaLogo from '../assets/partners/peika_bg.jpg';
 import vipMagazineLogo from '../assets/partners/VIP_Magazine_logo.png';
 
 // Industry
+import telerikLogo from '../assets/partners/telerik_academy.png';
 import codingGirlsLogo from '../assets/partners/coding_girls.png';
 
 
@@ -104,17 +106,22 @@ class Partners extends React.Component {
                     <div className="row">
                         <div className="col-md-6 text-center">
                             <div className="partner">
+                                <a href="https://www.centroida.ai"><img src={centroidaLogo} alt="Centroida" /></a>
+                            </div>
+                        </div>
+                        <div className="col-md-6 text-center">
+                            <div className="partner">
                                 <a href="http://www.coresnet.bg"><img src={coresNetworksLogo} alt="Cores Networks" /></a>
                             </div>
                         </div>
+                    </div>
+                    <div className="row">
                         <div className="col-md-6 text-center">
                             <div className="partner">
                                 <a href="https://www.sbtech.com"><img src={sbtechLogo} alt="SBTech" /></a>
                             </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-6 col-md-offset-3 text-center">
+                        <div className="col-md-6 text-center">
                             <div className="partner">
                                 <a href="https://strypes.eu"><img src={strypesLogo} alt="Strypes" /></a>
                             </div>
