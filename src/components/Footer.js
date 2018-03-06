2e8f12749effe804215446697c1dadf5909ca704
Nikola Lazarov <nikola.lazarov@protonmail.com>
2018-03-06 14:11:17 +0200

    add twitter to the social menu

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import YoutubeIcon from '../assets/icon_youtube.svg';
 import FacebookIcon from '../assets/icon_facebook.svg';
+import TwitterIcon from '../assets/icon_twitter.svg';
 import InstagramIcon from '../assets/icon_instagram.svg';
 
 class Footer extends React.Component {
@@ -17,9 +18,10 @@ class Footer extends React.Component {
                             <div id="follow-us">
                                 <h3>Follow us</h3>
                                 <ul>
-                                    <li><a href="https://www.facebook.com/TheHubAUBG"><img src={FacebookIcon} alt="Facebook" /></a></li>
-                                    <li><a href="https://www.youtube.com/channel/UChdtBZBvaK9XZurP3GjPDug"><img src={YoutubeIcon} alt="Youtube" /></a></li>
-                                    <li><a href="https://www.instagram.com/thehubaubg"><img src={InstagramIcon} alt="Instagram" /></a></li>
+                                    <li><a href="https://www.facebook.com/TheHubAUBG" target="_blank"><img src={FacebookIcon} alt="Facebook" /></a></li>
+                                    <li><a href="https://twitter.com/thehubaubg" target="_blank"><img src={TwitterIcon} alt="Twitter" /></a></li>
+                                    <li><a href="https://www.youtube.com/channel/UChdtBZBvaK9XZurP3GjPDug" target="_blank"><img src={YoutubeIcon} alt="Youtube" /></a></li>
+                                    <li><a href="https://www.instagram.com/thehubaubg" target="_blank"><img src={InstagramIcon} alt="Instagram" /></a></li>
                                 </ul>
                             </div>
                         </div>
